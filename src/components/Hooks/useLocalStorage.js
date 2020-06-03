85ff0556a70c5b2098ac8a32b26a414425dcd900
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-06-03 16:27:25 -0400

    fixed little bug

--- src/components/Hooks/useLocalStorage.js
+++ src/components/Hooks/useLocalStorage.js
@@ -5,14 +5,14 @@ export const useLocalStorage =(key, initialValue)=>{
     const [storedValue, setStoredValue]=useState(()=>{
        
             const mode = window.localStorage.getItem(key);
-            return mode? mode : initialValue;
+            return mode? JSON.parse(mode) : initialValue;
     })
         
 
 
    const setValue=(value)=>{
         setStoredValue(value);
-        window.localStorage.setItem(key,value);
+        window.localStorage.setItem(key,JSON.stringify(value));
     };
  
    return [storedValue, setValue];
